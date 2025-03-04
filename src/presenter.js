c399f3a71713e6103d1ff1a20c763630316667ba
Isabella12-bit <isabella.montes@ucb.edu.bo>
2025-03-03 23:37:55 -0400

    Fix: Antes al apretar el botón no aparecía el número convertido debido a un par de errores que ya fueron solucionados

--- src/presenter.js
+++ src/presenter.js
@@ -1,6 +1,6 @@
-import { decimalToRoman } from "./sumador.js";
+import { decimalARomano } from "./sumador.js";
 
 document.getElementById("convertir").addEventListener("click", () => {
     const numero = parseInt(document.getElementById("numero").value, 10);
-    document.getElementById("resultado").textContent = decimalToRoman(numero);
+    document.getElementById("resultado").textContent = decimalARomano(numero);
 });
