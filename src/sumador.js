3debda39c14ce811914bbc3bef411a879bc1a4e5
Isabella12-bit <isabella.montes@ucb.edu.bo>
2025-03-03 22:58:49 -0400

    Feat: Ahora sirve hasta el 20

--- src/sumador.js
+++ src/sumador.js
@@ -6,15 +6,14 @@ export function decimalARomano(num) {
     { valor: 4, simbolo: "IV" },
     { valor: 1, simbolo: "I" }
   ];
-
+  
   let resultado = "";
-
   for (let i = 0; i < valores.length; i++) {
     while (num >= valores[i].valor) {
       resultado += valores[i].simbolo;
       num -= valores[i].valor;
     }
   }
-
+  
   return resultado;
 }
--- src/sumador.test.js
+++ src/sumador.test.js
@@ -11,6 +11,15 @@ test("Convierte 2 a II", () => {
 test("Convierte 3 a III", () => {
   expect(decimalARomano(3)).toBe("III");
 });
+
 test("Convierte 7 a VII", () => {
   expect(decimalARomano(7)).toBe("VII");
+});
+
+test("Convierte 15 a XV", () => {
+  expect(decimalARomano(15)).toBe("XV");
+});
+
+test("Convierte 20 a XX", () => {
+  expect(decimalARomano(20)).toBe("XX");
 });
